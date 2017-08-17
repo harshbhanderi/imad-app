ece35e7bfc380156e9b9c378b984de9453c19ad4
harshbhanderi <hbhnderi@gmail.com>
2017-08-17 19:13:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/hello', function (req, res) {
-    res.send("hello")
+    res.sendFile(path.join(__dirname, 'ui', 'hello.html'));
 })
 
 app.get('/ui/style.css', function (req, res) {
